0272be5c394bc8ed434a6f1a19a30a7c84b97e9b
vatsala <vatsala.swaroop@gmail.com>
2016-01-03 23:17:46 +0530

    Url configuration

--- petcare/petcare/urls.py
+++ petcare/petcare/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include,url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'petcare.view.home', name='home')
+    url(r'^vets/', include ('vets.urls')),
+    url(r'^$', 'petcare.view.home', name='home'),
 
 ]
