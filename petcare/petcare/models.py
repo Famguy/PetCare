333eee40adb8c8fac955a12a8750568087b808d4
Sagun Pai <sagung.pai@gmail.com>
2016-01-03 18:04:27 +0530

    modified models

--- petcare/petcare/models.py
+++ petcare/petcare/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class MushroomSpot(models.Model):
 
-    geom = PointField()
+	geom = PointField()
+	description = models.TextField()
+
+	@property
+	def popupContent(self):
+		return '<p>{}</p>'.format(
+			self.description)
