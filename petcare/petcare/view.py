339ddd8efec31c29c9f8154807fe4ef35f780783
Sagun Pai <sagung.pai@gmail.com>
2016-01-11 01:59:01 +0530

    slight mod

--- petcare/petcare/view.py
+++ petcare/petcare/view.py
@@ -6,5 +6,19 @@ def home(request):
 
 	
 	mushroomspot = MushroomSpot.objects.all()
-	print mushroomspot
+	ms_geom = {
+  "type":"Point",
+  "coordinates":[
+ -33, 150
+  ]
+}
+	ms_desp = "haha"
+	ms = MushroomSpot()
+	ms.geom = ms_geom
+	ms.description = ms_desp
+#	qs = MushroomSpot.objects.filter(id__in=custom_list)
+
+	for i in mushroomspot:
+		print i.geom
+		print i.description
 	return render(request, 'index.html', {'mushroomspot': mushroomspot})
