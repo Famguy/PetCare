c82b27bcc1c34872de4399708976304b33e510f1
Sagun Pai <sagung.pai@gmail.com>
2016-01-11 21:38:30 +0530

    modified models

--- petcare/vets/models.py
+++ petcare/vets/models.py
@@ -1,9 +1,18 @@
 # Create your models here.
 
-from djgeojson.fields import PointField
+#from geoposition.fields import GeopositionField
 from django.db import models
 
 class VetSpot(models.Model):
 
-	geom = PointField()
-	description = models.TextField()
+	name = models.CharField(max_length=100)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	address = models.TextField()
+#	phone = models.TextField()
+#	opennow = models.BooleanField()
+
+
+	class Meta:
+		verbose_name_plural = 'vet spots'
+
