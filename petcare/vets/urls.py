bd866444ab857556652a36ceb16a0d531f8bef99
vatsala <vatsala.swaroop@gmail.com>
2016-01-17 18:38:02 +0530

    Rearranged for default caching

--- petcare/vets/urls.py
+++ petcare/vets/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^find/',views.search,name='search'),
-    url(r'^searchtomap/',views.searchtomap,name='searchtomap'),   
+    url(r'^searchtomap/',views.searchtomap,name='searchtomap'), 
+    url(r'^locate/(?P<lat>\d+)',views.url_extract,name='check'),  
     url(r'^locate/',views.locate_me,name='locate'),
-    url(r'^locate/(?P<lat>\d+)',views.url_extract,name='check'),
+
 
 ]
