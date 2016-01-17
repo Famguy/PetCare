627a6b2e239dde2c3584a95eae90643fb2a9c481
vatsala <vatsala.swaroop@gmail.com>
2016-01-17 19:24:22 +0530

    Clubbing functionalities in one template

--- petcare/vets/urls.py
+++ petcare/vets/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^find/',views.search,name='search'),
     url(r'^searchtomap/',views.searchtomap,name='searchtomap'), 
-    url(r'^locate/(?P<lat>\d*[.]?\d+)/(?P<lon>\d*[.]?\d+)',views.url_extract,name='check'),  
-    url(r'^locate/',views.locate_me,name='locate'),
-
+    url(r'^(?P<lat>\d*[.]?\d+)/(?P<lon>\d*[.]?\d+)',views.url_extract,name='check'),  
 
 ]
